9273ca6e53a108ac8b51da456ebbae654262b13f
JuDaOcVa <juandavidocampo80@gmail.com>
2024-02-01 09:08:37 -0500

    Init commit repository with code

---
+++ Prj_JDOV_IMemoryCacheMVCApp/CacheKey.cs
@@ -0,0 +1,7 @@
+﻿namespace Prj_JDOV_IMemoryCacheMVCApp
+{
+    public static class CacheKey
+    {
+        public static String Entry { get { return "_EntryKey"; } }
+    }
+}
---
+++ Prj_JDOV_IMemoryCacheMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prj_JDOV_IMemoryCacheMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
